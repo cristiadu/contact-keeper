20a1adb9e437f606a88db4fa554fda2fe755cfa7
Cristiano Faustino <cristiadu@gmail.com>
2019-10-05 01:35:18 -0700

    adding initial code for the client

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const startServer = () => {
     app.use('/api/auth', require('./routes/AuthRoutes'));
     app.use('/api/contacts', require('./routes/ContactRoutes'));
     
-    const PORT = process.env.PORT || 3000;
+    const PORT = process.env.PORT || 5000;
     app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 };
 
